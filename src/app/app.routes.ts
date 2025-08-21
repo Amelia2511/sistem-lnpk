6351dfd0f40048e507f33f626de4c364eb81a8b1
Amelia2511 <amelia.aulia25@gmail.com>
2025-08-21 15:53:29 +0800

    new update - routerlink

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { LamanUtamaComponent } from './laman-utama/laman-utama.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'log-masuk', component: LoginPageComponent },
+    { path: 'laman-utama', component: LamanUtamaComponent },
+    // { path: '', redirectTo: '/laman-utama', pathMatch: 'full' }
+];
