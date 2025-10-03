0fdcd9b134fa509fcbe75c0aa182ad95842f3d59
nabilanmz <nabilanmzaid@gmail.com>
2025-10-03 10:22:38 +0800

    Change components theme (amber)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,9 +5,28 @@ import { providePrimeNG } from 'primeng/config';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import Aura from '@primeng/themes/aura';
+import { definePreset } from '@primeng/themes';
 import { provideNativeDateAdapter } from '@angular/material/core'
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
+
+const AuraAmberPreset = definePreset(Aura, {
+    semantic: {
+        primary: {
+            50: '{amber.50}',
+            100: '{amber.100}',
+            200: '{amber.200}',
+            300: '{amber.300}',
+            400: '{amber.400}',
+            500: '{amber.500}',
+            600: '{amber.600}',
+            700: '{amber.700}',
+            800: '{amber.800}',
+            900: '{amber.900}',
+            950: '{amber.950}'
+        }
+    }
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,7 +35,7 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     providePrimeNG({
       theme: {
-        preset: Aura
+        preset: AuraAmberPreset
         }
     }),
     provideZoneChangeDetection({ eventCoalescing: true }),
